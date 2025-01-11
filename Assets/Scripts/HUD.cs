91847c66e4b202af5d59bce8cb633fa00e60c634
Radi3ntal <ojoryan1@gmail.com>
2025-01-11 12:47:02 -0500

    a

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+
+    public Text remainingText;
+    public Text remainingSubtext;
+    public Text targetText;
+    public Text targetSubtext;
+
+    public Image[] stars;
+}
