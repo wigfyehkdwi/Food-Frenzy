b7afd75f54b97c3b926d53585e5d4d9a07529893
Radi3ntal <ojoryan1@gmail.com>
2025-01-11 12:55:21 -0500

    a

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HUD : MonoBehaviour
 {
     public Level level;
 
-    public Text remainingText;
-    public Text remainingSubtext;
-    public Text targetText;
-    public Text targetSubtext;
+    public TextMeshProUGUI remainingText;
+    public TextMeshProUGUI remainingSubtext;
+    public TextMeshProUGUI targetText;
+    public TextMeshProUGUI targetSubtext;
 
     public Image[] stars;
+
+    private void Start()
+    {
+        UpdateStars();
+    }
+
+    public void UpdateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].enabled = i == starIndex;
+        }
+    }
 }
